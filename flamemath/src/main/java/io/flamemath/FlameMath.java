df180b5bd33e8b5bff2fdfcd4445699c59017145
clueless-skywatcher <somichat@gmail.com>
2026-03-22 15:00:50 +0200

    Updated version 1.1.1

--- flamemath/src/main/java/io/flamemath/FlameMath.java
+++ flamemath/src/main/java/io/flamemath/FlameMath.java
@@ -10,7 +10,7 @@ import io.flamemath.expr.Expr;
 import io.flamemath.lang.parser.FlameParser;
 
 public class FlameMath {
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
 
     public static void main(String[] args) throws Exception {
         FlameValuator eval = new FlameValuator();
